cbace36a297f6de859ca9d1cb3f635af2f7c93c7
googlik935 <s.kiroman@yandex.ru>
2018-12-20 15:39:55 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,25 @@
-<?php
-
-  $age = htmlspecialchars ($_POST[age]);
-
-  if ($age < 30) {
-    echo "Вам меньше 30";
-  }
-  elseif ($age > 30) {
-    echo "Вам больше 30 лет";
-  }
-  elseif ($age = 30) {
-    echo "Вам 30 лет";
-  }
-
- ?>
-
  <form class="" action="index.php" method="post">
    <p>
-     Сколько вам лет?: <br>
+     Введите число: <br>
      <input type="text" name="age" value="">
    </p>
- </form>
+  </form>
+
+<?php
+
+
+  $age = htmlspecialchars($_POST['num']);
+
+   switch ($num) {
+     case 3:
+       echo "Три";
+       break;
+     case 5:
+      echo "Пять";
+       break;
+     default:
+       echo "Нет представления данного числа";
+       break;
+   }
+   
+?>
